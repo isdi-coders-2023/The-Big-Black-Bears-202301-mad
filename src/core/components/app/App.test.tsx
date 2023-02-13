3e1122ca1048160bc47abc1ac5d1ec1661a4268e
Fabio <fabiodn.798@gmail.com>
2023-02-13 19:59:05 +0100

    🐛 Solve sonar bug

--- src/core/components/app/App.test.tsx
+++ src/core/components/app/App.test.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
-import { AppRouter } from "../app.router/app.router";
 import Footer from "../footer/footer";
 
 jest.mock("../footer/footer");
