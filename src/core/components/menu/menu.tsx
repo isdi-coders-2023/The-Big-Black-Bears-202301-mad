19104e69d56fdcb3e48dde1dbeef3ab0448059fa
Ryan <ryanlearyzafra@gmail.com>
2023-02-13 20:08:45 +0100

    ✨ Add menu file

---
+++ src/core/components/menu/menu.tsx
@@ -0,0 +1,19 @@
+import "./menu.scss";
+export function Menu() {
+  return (
+    <ul className="burguermenu__list">
+      <li>
+        <a href="">Home</a>
+      </li>
+      <li>
+        <a href="">About</a>
+      </li>
+      <li>
+        <a href="">Characters</a>
+      </li>
+      <li>
+        <a href="">Favourites</a>
+      </li>
+    </ul>
+  );
+}
