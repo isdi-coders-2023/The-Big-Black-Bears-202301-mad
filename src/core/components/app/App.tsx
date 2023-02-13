0e9db33959c1422414dfbb5447024f48976171d9
Ryan <ryanlearyzafra@gmail.com>
2023-02-13 19:06:45 +0100

    🚀 Add footer to app file

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Footer from "../footer/footer";
 
 function App() {
   return (
     <div className="App">
       <h2>Jefe de equipo</h2>
+      <Footer />
     </div>
   );
 }
