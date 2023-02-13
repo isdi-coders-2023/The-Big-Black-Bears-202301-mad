52993f769415bf78131a84ea1afecdd87b320ea6
Fabio <fabiodn.798@gmail.com>
2023-02-13 19:50:42 +0100

    🚩 Add app.router

---
+++ src/core/components/app.router/app.router.tsx
@@ -0,0 +1,39 @@
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "../../../features/home/page/home";
+
+export type MenuOption = {
+  label: string;
+  path: string;
+};
+
+const menuOptions: MenuOption[] = [
+  { label: "Home", path: "/home" },
+  { label: "Characters", path: "/characters" },
+  { label: "Favorites", path: "/favorites" },
+  { label: "Details", path: "/details" },
+  { label: "Add", path: "/add" },
+  { label: "Modify", path: "/modify" },
+  { label: "About", path: "/about" },
+  { label: "Error", path: "/error" },
+];
+
+type AppRouterProps = {
+  menuOptions: MenuOption[];
+};
+
+export function AppRouter({ menuOptions }: AppRouterProps) {
+  console.log("create appRouter");
+  return (
+    <Routes>
+      <Route path={"/"} element={<HomePage></HomePage>}></Route>
+      <Route path={menuOptions[0].path} element={<HomePage></HomePage>}></Route>
+      {/* <Route path={menuOptions[1].path} element={<Characters></Characters>}></Route>
+      <Route path={menuOptions[2].path} element={<Favorites></Favorites>}></Route>
+      <Route path={menuOptions[3].path} element={<Details></Details>}></Route>
+      <Route path={menuOptions[4].path} element={<Add></Add>}></Route>
+      <Route path={menuOptions[5].path} element={<Modify></Modify>}></Route>
+      <Route path={menuOptions[6].path} element={<About></About>}></Route>
+      <Route path={menuOptions[7].path} element={<Error></Error>}></Route> */}
+    </Routes>
+  );
+}
