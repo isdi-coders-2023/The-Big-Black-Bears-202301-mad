6f204d0a9d40c7041a2a0faca058fe73e929a32f
Ariadna Frias <ariadna.812@gmail.com>
2023-02-13 23:10:09 +0100

    Update header tsx file

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -3,16 +3,26 @@ export function Header() {
   return (
     <header className="header">
       <div className="logos">
-        <img
-          className="logos__logo1"
-          src="../img/disney.png"
-          alt="Disney logo"
-        />
-        <img
-          className="logos__logo2"
-          src="../img/burguer.png"
-          alt="Burguer logo"
-        />
+        <div className="logos-desktop">
+          <img
+            className="logos__logo1"
+            src="../img/disney.png"
+            alt="Disney logo"
+          />
+          <img
+            className="logos__logo2"
+            src="../img/disney-logo.png"
+            alt="Disney logo"
+          />
+        </div>
+
+        <div>
+          <img
+            className="logos__logo3"
+            src="../img/burger.png"
+            alt="Burger logo"
+          />
+        </div>
       </div>
     </header>
   );
