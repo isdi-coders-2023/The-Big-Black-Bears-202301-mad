cf12787ff28f804a97b2495de6ee0abc576a78bf
Ryan <ryanlearyzafra@gmail.com>
2023-02-13 20:11:15 +0100

    Add menu to app file

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Footer from "../footer/footer";
+import { Menu } from "../menu/menu";
 
 function App() {
   return (
     <div className="App">
       <h2>Jefe de equipo</h2>
+      <Menu></Menu>
       <Footer />
     </div>
   );
