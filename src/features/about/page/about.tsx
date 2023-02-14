419f0d6665769281410c9d28c1844e97cdc3b6e3
Ariadna Frias <ariadna.812@gmail.com>
2023-02-14 13:55:52 +0100

    Update about tsx file

--- src/features/about/page/about.tsx
+++ src/features/about/page/about.tsx
@@ -1,24 +1,30 @@
+import "./about.scss";
+
 export default function AboutPage() {
   return (
-    <section>
-      <h2>About Us</h2>
-      <div>
-        <p>Welcome to our fancy app!</p>
+    <section className="about">
+      <h2 className="about__title">About us</h2>
+      <div className="about-containers">
+        <div className="about__div1">
+          <p>Welcome to our fancy app!</p>
+        </div>
+        <div className="about__div2">
+          <p>
+            This project is made with the purpose to make the Disney brand known
+            worldwide.
+          </p>
+        </div>
+        <div className="about__div3">
+          <p>
+            Our mission is to gather all the information related with all the
+            characters and make it available for all our users.
+          </p>
+        </div>
+        <div className="about__div4">
+          Take a look at some of them and find out in which video games and
+          films they have appeared.
+        </div>
       </div>
-      <div>
-        <p>
-          This project is made with the purpose to make the Disney brand known
-          worldwide.
-        </p>
-      </div>
-      <div>
-        <p>
-          Our mission is to gather all the information related with all the
-          characters and make it available for all our users.
-        </p>
-      </div>
-      Take a look at some of them and find out in which video games and films
-      they have appeared.
     </section>
   );
 }
