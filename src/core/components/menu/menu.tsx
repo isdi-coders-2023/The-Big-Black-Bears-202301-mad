c7e40561b3d942d0db4458a03e22bf28bd8afeb5
Ryan <ryanlearyzafra@gmail.com>
2023-02-14 15:58:54 +0100

    ✨ Add menu file

--- src/core/components/menu/menu.tsx
+++ src/core/components/menu/menu.tsx
@@ -1,42 +1,34 @@
-import { slide as Menu } from "react-burger-menu";
 import "./menu.scss";
-import {
-  BiHomeAlt,
-  BiUser,
-  BiMedal,
-  BiLogInCircle,
-  BiLogOutCircle,
-} from "react-icons/bi";
+import { BiHomeAlt, BiGroup, BiStar, BiUserCircle } from "react-icons/bi";
 
-type SidemenuProps = {
-  pageWrapId: string;
-  outerContainerId: string;
-};
-
-export const Sidebar = ({ pageWrapId, outerContainerId }: SidemenuProps) => {
+export const Menu = () => {
   return (
-    <Menu>
-      <p className="memu-title">Menu</p>
-      <a className="menu-item" href="/">
-        <BiHomeAlt />
-        Home
-      </a>
-      <a className="menu-item" href="/user">
-        <BiUser />
-        User
-      </a>
-      <a className="menu-item" href="/result">
-        <BiMedal />
-        Result
-      </a>
-      <a className="menu-item" href="/login">
-        <BiLogInCircle />
-        Log in
-      </a>
-      <a className="menu-item logout" href="/logout">
-        <BiLogOutCircle />
-        Log out
-      </a>
-    </Menu>
+    <>
+      <div className="menu_burger">
+        <img
+          className="logos__logo3"
+          src="../img/burger.png"
+          alt="Burger logo"
+        />
+        <div className="menu_paths">
+          <a className="menu-item" href="/">
+            <BiHomeAlt />
+            Home
+          </a>
+          <a className="menu-item" href="/user">
+            <BiGroup />
+            About
+          </a>
+          <a className="menu-item" href="/result">
+            <BiUserCircle />
+            Characters
+          </a>
+          <a className="menu-item" href="/login">
+            <BiStar />
+            Favourites
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
