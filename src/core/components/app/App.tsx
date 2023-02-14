b83c83e4d40ac3b3ac420d95edb5806d49a562ad
Ariadna Frias <ariadna.812@gmail.com>
2023-02-14 13:55:28 +0100

    Update app tsx

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Header } from "../header/header";
 import "./App.css";
 import Footer from "../footer/footer";
-import { HomePage } from "../../../features/home/page/home";
+
+import AboutPage from "../../../features/about/page/about";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <HomePage></HomePage>
+
       <Footer />
     </div>
   );
