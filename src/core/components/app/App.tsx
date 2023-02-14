e9704afc8e9019129f0edc131b20cbc2e7b29036
Ryan <ryanlearyzafra@gmail.com>
2023-02-14 16:09:54 +0100

    Adapt merge files

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -2,12 +2,33 @@ import React from "react";
 import { Header } from "../header/header";
 import Footer from "../footer/footer";
 import { HomePage } from "../../../features/home/page/home";
+import { Menu } from "../menu/menu";
+import { AppRouter } from "../app.router/app.router";
+
+export type MenuOption = {
+  label: string;
+  path: string;
+};
+
+export const menuOptions: MenuOption[] = [
+  { label: "Home", path: "/home" },
+  { label: "Characters", path: "/characters" },
+  { label: "details", path: "/details" },
+  { label: "Favorites", path: "/favorites" },
+  { label: "Add", path: "/add" },
+  { label: "Edit", path: "/edit" },
+  { label: "About", path: "/about" },
+  { label: "Error", path: "/error" },
+];
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header>
+        <Menu></Menu>
+      </Header>
       <HomePage></HomePage>
+      <AppRouter menuOptions={menuOptions}></AppRouter>
       <Footer />
     </div>
   );
