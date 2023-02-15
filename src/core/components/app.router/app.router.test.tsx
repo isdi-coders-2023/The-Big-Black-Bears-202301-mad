68c973c97afd9f54d3d591c03c0dd729113192f5
Ryan <ryanlearyzafra@gmail.com>
2023-02-15 14:11:02 +0100

    ✅ Update app router coverage

--- src/core/components/app.router/app.router.test.tsx
+++ src/core/components/app.router/app.router.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-await-sync-query */
 import { render, screen } from "@testing-library/react";
 import { AppRouter } from "./app.router";
 import { MemoryRouter as Router } from "react-router-dom";
@@ -9,8 +10,8 @@ const mockOptions: MenuOption[] = [
     path: "/home",
   },
   {
-    label: "1",
-    path: "/1",
+    label: "About",
+    path: "/about",
   },
   {
     label: "2",
@@ -20,13 +21,24 @@ const mockOptions: MenuOption[] = [
 
 describe("Given AppRouter", () => {
   describe("When the route is home", () => {
-    render(
-      <Router initialEntries={["/home", "/otra"]} initialIndex={0}>
-        <AppRouter menuOptions={mockOptions}></AppRouter>
-      </Router>
-    );
     test("Then we should navigate to home", async () => {
+      render(
+        <Router initialEntries={["/home", "/otra"]} initialIndex={0}>
+          <AppRouter menuOptions={mockOptions}></AppRouter>
+        </Router>
+      );
       const element = await screen.findByRole("link");
+      expect(element).toBeInTheDocument();
+    });
+  });
+  describe("When the route is about", () => {
+    test("Then we should navigate to about", async () => {
+      render(
+        <Router initialEntries={["/about", "/otra"]} initialIndex={0}>
+          <AppRouter menuOptions={mockOptions}></AppRouter>
+        </Router>
+      );
+      const element = await screen.findByText(/Welcome/i);
       expect(element).toBeInTheDocument();
     });
   });
