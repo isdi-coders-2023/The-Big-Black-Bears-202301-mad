aef5cd7ba144ae08f3fe2dfe6632c1dccc3ba244
Ariadna Frias <ariadna.812@gmail.com>
2023-02-17 18:03:47 +0100

    Update characters test

--- src/features/disneychar/characters/components/characters/characters.test.tsx
+++ src/features/disneychar/characters/components/characters/characters.test.tsx
@@ -4,10 +4,14 @@ import { MemoryRouter as Router } from "react-router-dom";
 
 describe("Given the characters page", () => {
   describe("When it is render", () => {
-    test("Then it should return the following images", () => {
-      render(<Characters></Characters>);
-      const elements = screen.getAllByRole("img");
-      expect(elements[0]).toBeInTheDocument();
+    test("Then it should return the following text", () => {
+      render(
+        <Router>
+          <Characters></Characters>
+        </Router>
+      );
+      const element = screen.getByText("Our characters");
+      expect(element).toBeInTheDocument();
     });
   });
 });
