97276330f8d16f28b80f5e7c65d8f1fb7c922b10
Fabio <fabiodn.798@gmail.com>
2023-02-17 18:45:53 +0100

    ✨ Create reducer for public api

---
+++ src/features/disneychar/characters/public.reducer/char.public.reducer.ts
@@ -0,0 +1,15 @@
+import { CharStructure } from "../models/character";
+import { charPublicAction } from "./char.public.actions";
+import { CharPublicAction } from "./char.public.actions.creators";
+
+export function charPublicReducer(
+  state: CharStructure[],
+  action: CharPublicAction
+): CharStructure[] {
+  if (action.type === charPublicAction.load) {
+    return action.payload as CharStructure[];
+  }else{
+
+  return state;
+  }
+}
