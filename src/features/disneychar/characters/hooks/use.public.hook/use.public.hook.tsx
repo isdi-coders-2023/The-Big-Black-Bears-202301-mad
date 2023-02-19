a3f496f935dee4154443ff0baade90e60bc14bbd
Ariadna Frias <ariadna.812@gmail.com>
2023-02-19 21:22:04 +0100

    Add public hook

--- src/features/disneychar/characters/hooks/use.public.hook/use.public.hook.tsx
+++ src/features/disneychar/characters/hooks/use.public.hook/use.public.hook.tsx
@@ -3,7 +3,6 @@ import { CharStructure } from "../../models/character";
 import { CharApiPublicRepo } from "../../services/publicapi/char.api.public.repo";
 import * as ac from "../../public.reducer/char.public.actions.creators";
 import { charPublicReducer } from "../../public.reducer/char.public.reducer";
-import { charPublicAction } from "../../public.reducer/char.public.actions";
 
 export type usePublicCharStructure = ReturnType<typeof usePublicChar>;
 export function usePublicChar(repo: CharApiPublicRepo) {
@@ -15,14 +14,17 @@ export function usePublicChar(repo: CharApiPublicRepo) {
     console.log(error.message);
   };
 
-  const loadPublicChar = useCallback(async (page: number) => {
-    try {
-      const char = await repo.loadChar(page);
-      dispatch(ac.loadPublicChar(char));
-    } catch (error) {
-      handlerError(error as Error);
-    }
-  }, [repo]);
+  const loadPublicChar = useCallback(
+    async (page: number) => {
+      try {
+        const char = await repo.loadChar(page);
+        dispatch(ac.loadPublicChar(char));
+      } catch (error) {
+        handlerError(error as Error);
+      }
+    },
+    [repo]
+  );
 
   return {
     char,
