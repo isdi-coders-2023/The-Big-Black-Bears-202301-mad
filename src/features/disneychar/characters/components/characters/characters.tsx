369e5b47f9e8ed43f85305f81700d27d711c1d58
Ariadna Frias <ariadna.812@gmail.com>
2023-02-19 21:21:48 +0100

    Add characters files

--- src/features/disneychar/characters/components/characters/characters.tsx
+++ src/features/disneychar/characters/components/characters/characters.tsx
@@ -4,42 +4,27 @@ import { Card } from "../card/card";
 import { useContext, useEffect, useState } from "react";
 import { CharsContext } from "../../context/characters.context";
 import { CharStructure } from "../../models/character";
+import { Filter } from "../filter/filter";
 
-type buttonProps = {
-  children: JSX.Element;
-};
-
-export default function Characters({ children }: buttonProps) {
+export default function Characters() {
   const { remote } = useContext(CharsContext);
 
   const [page, setPage] = useState(1);
   const [characters, setCharacters] = useState<CharStructure[]>([]);
-  const [films, setFilms] = useState<CharStructure[]>([]);
-  const [tvShows, setTvShows] = useState<CharStructure[]>([]);
-  const [videoGames, setVideoGames] = useState<CharStructure[]>([]);
-  const [shortFilms, setShortFilms] = useState<CharStructure[]>([]);
   const [displayedCharacters, setDisplayedCharacters] = useState<
     CharStructure[]
   >([]);
-  const [selectedCategory, setSelectedCategory] = useState<string>("All");
 
   useEffect(() => {
     remote?.loadPublicChar(page)?.then(() => {
       // Do something after loading characters
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
   useEffect(() => {
     if (remote?.char) {
       setCharacters(remote.char);
-      setFilms(remote.char.filter((char) => char.category === "films"));
-      setTvShows(remote.char.filter((char) => char.category === "tvShows"));
-      setVideoGames(
-        remote.char.filter((char) => char.category === "videoGames")
-      );
-      setShortFilms(
-        remote.char.filter((char) => char.category === "shortFilms")
-      );
       setDisplayedCharacters(remote.char);
     }
   }, [remote]);
@@ -52,63 +37,35 @@ export default function Characters({ children }: buttonProps) {
     setPage((prevPage) => prevPage - 1);
   };
 
-  const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const category = event.target.value;
-    setSelectedCategory(category);
-
-    let filteredCharacters: CharStructure[] = [];
-
-    switch (category) {
-      case "films":
-        filteredCharacters = characters.filter((char) => char.films.length > 0);
-        break;
-      case "tvShows":
-        filteredCharacters = characters.filter(
-          (char) => char.tvShows.length > 0
-        );
-        break;
-      case "videoGames":
-        filteredCharacters = characters.filter(
-          (char) => char.videoGames.length > 0
-        );
-        break;
-      case "shortFilms":
-        filteredCharacters = characters.filter(
-          (char) => char.shortFilms.length > 0
-        );
-        break;
-      default:
-        // If "All" or an invalid category is selected, show all characters
-        filteredCharacters = characters;
-        break;
-    }
-
-    setDisplayedCharacters(filteredCharacters);
-  };
-
   return (
     <section className="characters">
       <h2 className="characters__title">Our characters</h2>
-      <span className="characters__category">Category</span>
-      <select
-        className="characters__filter"
-        onChange={handleFilterChange}
-        value={selectedCategory}
-      >
-        <option value="All">All</option>
-        <option value="films">Films</option>
-        <option value="tvShows">TV Shows</option>
-        <option value="videoGames">Video Games</option>
-        <option value="shortFilms">Short Films</option>
-      </select>
+      <Filter
+        characters={characters}
+        setDisplayedCharacters={setDisplayedCharacters}
+      />
       <ul className="characters__list">
         {displayedCharacters.map((item: CharStructure) => (
           <Card key={item.id} character={item}></Card>
         ))}
       </ul>
-      <button onClick={handlePrevPage}>Prev Page</button>
-      <button onClick={handleNextPage}>Next Page</button>
-      {children}
+      <div className="characters__buttons">
+        <button id="prev" title="prev" onClick={handlePrevPage}>
+          <img
+            className="characters__prev"
+            src="../img/prev-button.svg"
+            alt="previous button"
+          ></img>
+        </button>
+        Page {page}
+        <button onClick={handleNextPage}>
+          <img
+            className="characters__next"
+            src="../img/next-button.svg"
+            alt="next button"
+          ></img>
+        </button>
+      </div>
     </section>
   );
 }
