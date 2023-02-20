cb7a91e74562ca0284460123a2074b41b2a9985e
Ariadna Frias <ariadna.812@gmail.com>
2023-02-20 23:28:10 +0100

    Update details test

--- src/features/disneychar/characters/components/details/details.test.tsx
+++ src/features/disneychar/characters/components/details/details.test.tsx
@@ -1,12 +1,12 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 import { act, render, screen } from "@testing-library/react";
 import { CharsContext } from "../../context/characters.context";
-
+import { usePublicCharStructure } from "../../hooks/use.public.hook/use.public.hook";
 import Details from "./details";
 import { MemoryRouter as Router } from "react-router-dom";
-import { usePublicCharStructure } from "../../hooks/use.public.hook/use.public.hook";
 
 const mockContext = {
-  characterList: [
+  charList: [
     { name: "test", id: 1 },
     { name: "test", id: 2 },
   ],
@@ -14,16 +14,21 @@ const mockContext = {
 
 describe("Given Details page component", () => {
   describe("When we render the component", () => {
-    test('Then, the heading <h2> "Details" should be in the document', () => {
-      // eslint-disable-next-line testing-library/no-unnecessary-act
-
-      render(
-        <Router>
-          <Details></Details>
-        </Router>
+    test('Then, the heading <h2> "Details of " should be in the document', async () => {
+      await act(async () =>
+        render(
+          <CharsContext.Provider value={mockContext}>
+            <Router
+              initialEntries={["/details/1", "/details/2"]}
+              initialIndex={0}
+            >
+              <Details></Details>
+            </Router>
+          </CharsContext.Provider>
+        )
       );
 
-      const element = screen.getByText(/Details/);
+      const element = screen.getByRole("heading");
 
       expect(element).toBeInTheDocument();
     });
